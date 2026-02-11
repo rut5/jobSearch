b459565b7bf35a0134ec50b1f34d84fffab5683e
Rut <rutwintzell@outlook.com>
2026-02-11 17:28:06 +0100

    Tried to install eslint

--- index.ts
+++ index.ts
@@ -2,17 +2,38 @@
 // A function that runs our app
 // the command to start everything
 
-const searchJobs = async (keyword: string) => {
+// Adding interfaces for defining types
+interface Employer {
+  name: string;
+}
+
+interface WorkplaceAddress {
+  municipality: string;
+}
+
+interface Job {
+  headline: string;
+  employer: Employer;
+  workplace_address: WorkplaceAddress;
+  publication_date: string;
+}
+
+interface JobSearchResult {
+  hits: Job[];
+}
+
+const searchJobs = async (keyword: string): Promise<void> => {
   try {
     const result = `https://jobsearch.api.jobtechdev.se/search?q=${keyword}&offset=0&limit=10`;
     const response = await fetch(result);
-    const data = await response.json();
+
+    const data: JobSearchResult = await response.json();
 
     console.log(`\nFound ${data.hits.length} jobs`);
     console.log("-".repeat(50));
     //console.log(data);
 
-    data.hits.forEach((job: any, index: number) => {
+    data.hits.forEach((job: Job, index: number) => {
       const pubDate = new Date(job.publication_date);
       //console.log("pubDate: ", pubDate);
 
